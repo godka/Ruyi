5735c4a57049cd231db9d4cc9d82bcbaa9ca2962
godka <htc19@mails.tsinghua.edu.cn>
2022-07-04 12:21:55 +0800

    ruyi

---
+++ src/abr.py
@@ -0,0 +1,122 @@
+# add queuing delay into halo
+import os
+import numpy as np
+import core
+import load_trace
+
+# bit_rate, buffer_size, next_chunk_size, bandwidth_measurement(throughput and time), chunk_til_video_end
+S_INFO = 8
+S_LEN = 8  # take how many frames in the past
+A_DIM = 6
+TRAIN_SEQ_LEN = 100  # take as a train batch
+MODEL_SAVE_INTERVAL = 100
+VIDEO_BIT_RATE = np.array([300., 750., 1200., 1850., 2850., 4300.])  # Kbps
+BUFFER_NORM_FACTOR = 10.0
+CHUNK_TIL_VIDEO_END_CAP = 48.0
+M_IN_K = 1000.0
+REBUF_PENALTY = 4.3  # 1 sec rebuffering -> 3 Mbps
+SMOOTH_PENALTY = 1
+DEFAULT_QUALITY = 1  # default video quality without agent
+RANDOM_SEED = 42
+RAND_RANGE = 1000
+EPS = 1e-6
+
+
+class ABREnv():
+
+    def __init__(self, random_seed=RANDOM_SEED):
+        np.random.seed(random_seed)
+        all_cooked_time, all_cooked_bw, _ = load_trace.load_trace()
+        self.net_env = core.Environment(all_cooked_time=all_cooked_time,
+                                          all_cooked_bw=all_cooked_bw,
+                                          random_seed=random_seed)
+
+        self.last_bit_rate = DEFAULT_QUALITY
+        self.last_vmaf = 0.
+        self.beta, self.gamma = np.random.uniform(), np.random.uniform()
+
+        self.buffer_size = 0.
+        self.state = np.zeros((S_INFO, S_LEN))
+        # self.reset()
+        
+    def seed(self, num):
+        np.random.seed(num)
+
+    def reset(self):
+        # self.net_env.reset_ptr()
+        self.time_stamp = 0
+        self.last_bit_rate = DEFAULT_QUALITY
+        self.state = np.zeros((S_INFO, S_LEN))
+        self.buffer_size = 0.
+        bit_rate = self.last_bit_rate
+        self.beta, self.gamma = np.random.uniform(), np.random.uniform()
+
+        self.state = np.zeros((S_INFO, S_LEN))
+        bit_rate = self.last_bit_rate
+        delay, sleep_time, buffer_size, rebuf, \
+            video_chunk_size, next_video_chunk_sizes, \
+            next_video_chunk_vmaf, vmaf, \
+            end_of_video, video_chunk_remain = \
+            self.net_env.get_video_chunk(bit_rate)
+        state = np.roll(self.state, -1, axis=1)
+
+        self.last_vmaf = vmaf
+
+        # this should be S_INFO number of terms
+        state[0, -1] = vmaf / 100.
+        # self.video_bitrate[bit_rate] / M_IN_K
+        state[1, -1] = buffer_size / BUFFER_NORM_FACTOR  # 10 sec
+        state[2, -1] = float(video_chunk_size) / float(delay) / M_IN_K  # kilo byte / ms
+        state[3, -1] = float(delay) / M_IN_K / BUFFER_NORM_FACTOR  # 10 sec
+        state[4] = -1.
+        state[4, :A_DIM] = np.array(next_video_chunk_sizes) / M_IN_K / M_IN_K  # mega byte
+        state[5] = -1.
+        state[5, :A_DIM] = np.array(next_video_chunk_vmaf) / 100.
+        state[6, -1] = np.minimum(video_chunk_remain, CHUNK_TIL_VIDEO_END_CAP) / float(CHUNK_TIL_VIDEO_END_CAP)
+        state[7, :2] = np.array([self.beta, self.gamma])
+        
+        self.state = state
+        return state
+        #return state.reshape((1, S_INFO*S_LEN))
+
+    def render(self):
+        return
+
+    def step(self, action):
+        bit_rate = int(action)
+        # the action is from the last decision
+        # this is to make the framework similar to the real
+        delay, sleep_time, buffer_size, rebuf, \
+            video_chunk_size, next_video_chunk_sizes, \
+            next_video_chunk_vmaf, vmaf, \
+            end_of_video, video_chunk_remain = \
+            self.net_env.get_video_chunk(bit_rate)
+
+
+        self.time_stamp += delay  # in ms
+        self.time_stamp += sleep_time  # in ms
+
+        reward = vmaf - self.beta * 100. * rebuf - \
+            self.gamma * 5. * np.abs(vmaf - self.last_vmaf)
+
+        self.last_bit_rate = bit_rate
+        self.last_vmaf = vmaf
+
+        state = np.roll(self.state, -1, axis=1)
+
+        # this should be S_INFO number of terms
+        state[0, -1] = vmaf / 100.
+        # self.video_bitrate[bit_rate] / M_IN_K
+        state[1, -1] = buffer_size / BUFFER_NORM_FACTOR  # 10 sec
+        state[2, -1] = float(video_chunk_size) / float(delay) / M_IN_K  # kilo byte / ms
+        state[3, -1] = float(delay) / M_IN_K / BUFFER_NORM_FACTOR  # 10 sec
+        state[4] = -1.
+        state[4, :A_DIM] = np.array(next_video_chunk_sizes) / M_IN_K / M_IN_K  # mega byte
+        state[5] = -1.
+        state[5, :A_DIM] = np.array(next_video_chunk_vmaf) / 100.
+        state[6, -1] = np.minimum(video_chunk_remain, CHUNK_TIL_VIDEO_END_CAP) / float(CHUNK_TIL_VIDEO_END_CAP)
+        state[7, :2] = np.array([self.beta, self.gamma])
+
+        self.state = state
+        #observation, reward, done, info = env.step(action)
+        return state, reward, end_of_video, {'bitrate': VIDEO_BIT_RATE[bit_rate], 'rebuffer': rebuf}
---
+++ src/dqn.py
@@ -0,0 +1,164 @@
+import math
+import numpy as np
+import tensorflow.compat.v1 as tf
+import os
+import time
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+import tflearn
+
+FEATURE_NUM = 128
+ACTION_EPS = 1e-4
+GAMMA = 0.99
+MAX_POOL_NUM = 500000
+TAU = 1e-5
+
+class Network():
+    def CreateTarget(self, inputs):
+        with tf.variable_scope('target'):
+            split_0 = tflearn.fully_connected(inputs[:, 0:1, -1], FEATURE_NUM, activation='relu')
+            split_1 = tflearn.fully_connected(inputs[:, 1:2, -1], FEATURE_NUM, activation='relu')
+            split_2 = tflearn.conv_1d(inputs[:, 2:3, :], FEATURE_NUM, 1, activation='relu')
+            split_3 = tflearn.conv_1d(inputs[:, 3:4, :], FEATURE_NUM, 1, activation='relu')
+            split_4 = tflearn.conv_1d(inputs[:, 4:5, :self.a_dim], FEATURE_NUM, 1, activation='relu')
+            split_5 = tflearn.conv_1d(inputs[:, 5:6, :self.a_dim], FEATURE_NUM, 1, activation='relu')
+            split_6 = tflearn.fully_connected(inputs[:, 6:7, -1], FEATURE_NUM, activation='relu')
+            split_7 = tflearn.fully_connected(inputs[:, 7:8, :2], FEATURE_NUM, activation='relu')
+
+            split_2_flat = tflearn.flatten(split_2)
+            split_3_flat = tflearn.flatten(split_3)
+            split_4_flat = tflearn.flatten(split_4)
+            split_5_flat = tflearn.flatten(split_5)
+
+            merge_net = tflearn.merge([split_0, split_1, split_2_flat, split_3_flat, split_4_flat, \
+                split_5_flat, split_6, split_7], 'concat')
+            net = tflearn.fully_connected(merge_net, FEATURE_NUM, activation='relu')
+            value = tflearn.fully_connected(net, self.a_dim, activation='linear') 
+            
+            return value
+
+    def CreateEval(self, inputs):
+        with tf.variable_scope('eval', reuse=tf.AUTO_REUSE):
+            split_0 = tflearn.fully_connected(inputs[:, 0:1, -1], FEATURE_NUM, activation='relu')
+            split_1 = tflearn.fully_connected(inputs[:, 1:2, -1], FEATURE_NUM, activation='relu')
+            split_2 = tflearn.conv_1d(inputs[:, 2:3, :], FEATURE_NUM, 1, activation='relu')
+            split_3 = tflearn.conv_1d(inputs[:, 3:4, :], FEATURE_NUM, 1, activation='relu')
+            split_4 = tflearn.conv_1d(inputs[:, 4:5, :self.a_dim], FEATURE_NUM, 1, activation='relu')
+            split_5 = tflearn.conv_1d(inputs[:, 5:6, :self.a_dim], FEATURE_NUM, 1, activation='relu')
+            split_6 = tflearn.fully_connected(inputs[:, 6:7, -1], FEATURE_NUM, activation='relu')
+            split_7 = tflearn.fully_connected(inputs[:, 7:8, :2], FEATURE_NUM, activation='relu')
+
+            split_2_flat = tflearn.flatten(split_2)
+            split_3_flat = tflearn.flatten(split_3)
+            split_4_flat = tflearn.flatten(split_4)
+            split_5_flat = tflearn.flatten(split_5)
+
+            merge_net = tflearn.merge([split_0, split_1, split_2_flat, split_3_flat, split_4_flat, \
+                split_5_flat, split_6, split_7], 'concat')
+            net = tflearn.fully_connected(merge_net, FEATURE_NUM, activation='relu')
+            value = tflearn.fully_connected(net, self.a_dim, activation='linear')
+            
+            return value
+            
+    def get_network_params(self):
+        return self.sess.run(self.network_params)
+
+    def update(self, tau = 1e-4):
+        eval_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='eval')
+        target_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target')
+        for (e_param, t_param) in zip(eval_params, target_params):
+            t_param.assign((1 - tau) * t_param + tau * e_param)
+        
+    def set_network_params(self, input_network_params):
+        self.sess.run(self.set_network_params_op, feed_dict={
+            i: d for i, d in zip(self.input_network_params, input_network_params)
+        })
+
+    def __init__(self, sess, state_dim, action_dim, learning_rate):
+        self.s_dim = state_dim
+        self.a_dim = action_dim
+        self.lr_rate = learning_rate
+        self.sess = sess
+        self.r = tf.placeholder(tf.float32, [None, 1])
+        self.done = tf.placeholder(tf.float32, [None, 1])
+
+        self.inputs = tf.placeholder(tf.float32, [None, self.s_dim[0], self.s_dim[1]])
+        self.ns_inputs = tf.placeholder(tf.float32, [None, self.s_dim[0], self.s_dim[1]])
+        
+        self.acts = tf.placeholder(tf.float32, [None, self.a_dim])
+        self.eval = self.CreateEval(inputs=self.inputs)
+        self.eval_ns = self.CreateEval(inputs=self.ns_inputs)
+        self.target = self.CreateTarget(inputs=self.ns_inputs)
+        self.max_target = tf.reduce_max(self.target, axis=-1)
+        self.double_target = tf.reduce_sum(tf.multiply(self.target, \
+                                tf.one_hot(tf.argmax(self.eval_ns, axis=-1), self.a_dim)), reduction_indices=1, keepdims=True)
+
+        self.R = tf.stop_gradient(self.r + GAMMA * (1 - self.done) * self.double_target)
+        self.pool = []
+
+        self.eval_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='eval')
+        self.target_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target')
+        self.soft_update = [tf.assign(ta, (1 - TAU) * ta + TAU * ea)
+                for ta, ea in zip(self.target_params, self.eval_params)]
+        
+        # Get all network parameters
+        self.network_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='eval')
+        self.network_params += \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target')
+
+        # Set all network parameters
+        self.input_network_params = []
+        for param in self.network_params:
+            self.input_network_params.append(
+                tf.placeholder(tf.float32, shape=param.get_shape()))
+        self.set_network_params_op = []
+        for idx, param in enumerate(self.input_network_params):
+            self.set_network_params_op.append(
+                self.network_params[idx].assign(param))
+        
+        self.loss = tflearn.mean_square(
+            tf.reduce_sum(tf.multiply(self.eval, self.acts), reduction_indices=1, keepdims=True),
+            self.R)
+        
+        self.val_opt = tf.train.AdamOptimizer(self.lr_rate).minimize(self.loss)
+
+    def predict(self, input):
+        action = self.sess.run(self.eval, feed_dict={
+            self.inputs: input
+        })
+        return action[0]
+
+    def train(self, s_batch, a_batch, p_batch, r_batch, d_batch, epoch):
+        # ns: next state
+        for (s, a, v, ns, d) in zip(s_batch, a_batch, r_batch, p_batch, d_batch):
+            if len(self.pool) > MAX_POOL_NUM:
+                pop_item = np.random.randint(len(self.pool))
+                self.pool[pop_item] = [s, a, v, ns, d]
+            else:
+                self.pool.append([s, a, v, ns, d])
+        
+        if len(self.pool) > 4096:
+            s_batch, a_batch, r_batch, ns_batch, d_batch = [], [], [], [], []
+
+            #for _ in range(512):
+            pop_items = np.random.randint(len(self.pool), size=1024)
+            for pop_item in pop_items:
+                s_, a_, r_, ns_, d_ = self.pool[pop_item]
+                s_batch.append(s_)
+                a_batch.append(a_)
+                r_batch.append(r_)
+                ns_batch.append(ns_)
+                d_batch.append(d_)
+
+            self.sess.run(self.val_opt, feed_dict={
+                self.inputs: s_batch,
+                self.acts: a_batch,
+                self.r: r_batch,
+                self.ns_inputs: ns_batch,
+                self.done: d_batch
+            })
+            self.sess.run(self.soft_update)
